176b3963e81cf02a876d9f66c4b93dd1326e32ea
Toolika32 <toolikanegi2531@gmail.com>
2026-08-18 18:44:23 +0530

    Updated retriever.py

--- Backend/retriever.py
+++ Backend/retriever.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004",google_api_key=os.getenv("GOOGLE_API_KEY")
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001", google_api_key=os.getenv("GOOGLE_API_KEY"))
 
 vectorstore = FAISS.load_local(
     "faiss_index",
